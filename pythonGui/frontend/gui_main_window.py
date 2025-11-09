138589a7dd732d11df9a29fc2688a6aa4e41879b
Ananth-Pai19 <paiananth0838@gmail.com>
2025-11-09 09:12:21 +0530

    Refactor GUI layout and update styles
    Updated GUI layout and styles for improved appearance and functionality.

--- pythonGui/frontend/gui_main_window.py
+++ pythonGui/frontend/gui_main_window.py
@@ -25,130 +25,259 @@ class Ui_guiMainWindow(object):
     def setupUi(self, guiMainWindow):
         if not guiMainWindow.objectName():
             guiMainWindow.setObjectName(u"guiMainWindow")
-        guiMainWindow.resize(802, 600)
+        guiMainWindow.resize(938, 640)
+        guiMainWindow.setStyleSheet(u"background-color: rgb(14, 14, 16);")
         self.centralwidget = QWidget(guiMainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.roverwheelwidget = RoverWheelWidget(self.centralwidget)
-        self.roverwheelwidget.setObjectName(u"widget")
-        self.roverwheelwidget.setGeometry(QRect(90, 70, 301, 421))
-        self.widget1 = QWidget(self.centralwidget)
-        self.widget1.setObjectName(u"widget1")
-        self.widget1.setGeometry(QRect(500, 20, 271, 531))
-        self.verticalLayout = QVBoxLayout(self.widget1)
+        self.widget = RoverWheelWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(90, 70, 301, 421))
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(500, 20, 361, 531))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.CurrentRoverStatus = QGroupBox(self.widget1)
+        self.CurrentRoverStatus = QGroupBox(self.layoutWidget)
         self.CurrentRoverStatus.setObjectName(u"CurrentRoverStatus")
+        self.CurrentRoverStatus.setStyleSheet(u"QGroupBox {\n"
+"	border: 2px solid rgb(227, 30, 3);\n"
+"	border-radius: 10px;\n"
+"	margin-top: 0.5ex;\n"
+"	font-size: 24px;\n"
+"	font-weight: bold;\n"
+"	font: 700 20pt \"Uroob\";\n"
+"}\n"
+"\n"
+"QGroupBox::title {\n"
+"	color: rgb(227, 30, 3);\n"
+"	subcontrol-origin: margin;\n"
+"	subcontrol-position: top-left;\n"
+"	padding: 0 10px;\n"
+"	font-weight: bold;\n"
+"}")
         self.gridLayout_3 = QGridLayout(self.CurrentRoverStatus)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.label_2 = QLabel(self.CurrentRoverStatus)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout_3.addWidget(self.label_2, 2, 2, 1, 1)
-
-        self.currentSettingNameLabel = QLabel(self.CurrentRoverStatus)
-        self.currentSettingNameLabel.setObjectName(u"currentSettingNameLabel")
-
-        self.gridLayout_3.addWidget(self.currentSettingNameLabel, 2, 0, 1, 2)
-
         self.modeNameLabel = QLabel(self.CurrentRoverStatus)
         self.modeNameLabel.setObjectName(u"modeNameLabel")
+        self.modeNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout_3.addWidget(self.modeNameLabel, 0, 0, 1, 1)
 
+        self.currentSettingNameLabel = QLabel(self.CurrentRoverStatus)
+        self.currentSettingNameLabel.setObjectName(u"currentSettingNameLabel")
+        self.currentSettingNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
+
+        self.gridLayout_3.addWidget(self.currentSettingNameLabel, 2, 0, 1, 1)
+
         self.label = QLabel(self.CurrentRoverStatus)
         self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
-        self.gridLayout_3.addWidget(self.label, 0, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.label, 0, 1, 1, 1)
+
+        self.label_2 = QLabel(self.CurrentRoverStatus)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
+
+        self.gridLayout_3.addWidget(self.label_2, 2, 1, 1, 1)
 
 
         self.verticalLayout.addWidget(self.CurrentRoverStatus)
 
-        self.sensorDataPanel = QGroupBox(self.widget1)
+        self.sensorDataPanel = QGroupBox(self.layoutWidget)
         self.sensorDataPanel.setObjectName(u"sensorDataPanel")
+        self.sensorDataPanel.setStyleSheet(u"QGroupBox {\n"
+"	border: 2px solid rgb(227, 30, 3);\n"
+"	border-radius: 10px;\n"
+"	margin-top: 0.5ex;\n"
+"	font-size: 24px;\n"
+"	font-weight: bold;\n"
+"	font: 700 20pt \"Uroob\";\n"
+"}\n"
+"\n"
+"QGroupBox::title {\n"
+"	color: rgb(227, 30, 3);\n"
+"	subcontrol-origin: margin;\n"
+"	subcontrol-position: top-left;\n"
+"	padding: 0 10px;\n"
+"	font-weight: bold;\n"
+"}")
         self.gridLayout = QGridLayout(self.sensorDataPanel)
         self.gridLayout.setObjectName(u"gridLayout")
         self.batteryNameLabel = QLabel(self.sensorDataPanel)
         self.batteryNameLabel.setObjectName(u"batteryNameLabel")
+        self.batteryNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.batteryNameLabel, 4, 0, 1, 1)
 
         self.batteryValueLabel = QLabel(self.sensorDataPanel)
         self.batteryValueLabel.setObjectName(u"batteryValueLabel")
+        self.batteryValueLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.batteryValueLabel, 4, 1, 1, 1)
 
         self.LatencyNameLabel = QLabel(self.sensorDataPanel)
         self.LatencyNameLabel.setObjectName(u"LatencyNameLabel")
+        self.LatencyNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.LatencyNameLabel, 3, 0, 1, 1)
 
         self.LatencyValueLabel = QLabel(self.sensorDataPanel)
         self.LatencyValueLabel.setObjectName(u"LatencyValueLabel")
+        self.LatencyValueLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.LatencyValueLabel, 3, 1, 1, 1)
 
         self.GPSNameLabel = QLabel(self.sensorDataPanel)
         self.GPSNameLabel.setObjectName(u"GPSNameLabel")
+        self.GPSNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.GPSNameLabel, 0, 0, 1, 1)
 
         self.LinearXNameLabel = QLabel(self.sensorDataPanel)
         self.LinearXNameLabel.setObjectName(u"LinearXNameLabel")
+        self.LinearXNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.LinearXNameLabel, 2, 0, 1, 1)
 
         self.YawValueLabel = QLabel(self.sensorDataPanel)
         self.YawValueLabel.setObjectName(u"YawValueLabel")
+        self.YawValueLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.YawValueLabel, 1, 1, 1, 1)
 
         self.yawNameLabel = QLabel(self.sensorDataPanel)
         self.yawNameLabel.setObjectName(u"yawNameLabel")
+        self.yawNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.yawNameLabel, 1, 0, 1, 1)
 
         self.LinearXValueLabel = QLabel(self.sensorDataPanel)
         self.LinearXValueLabel.setObjectName(u"LinearXValueLabel")
+        self.LinearXValueLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.LinearXValueLabel, 2, 1, 1, 1)
 
         self.GPSValuesLabel = QLabel(self.sensorDataPanel)
         self.GPSValuesLabel.setObjectName(u"GPSValuesLabel")
+        self.GPSValuesLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout.addWidget(self.GPSValuesLabel, 0, 1, 1, 1)
 
 
         self.verticalLayout.addWidget(self.sensorDataPanel)
 
-        self.canGroupBox = QGroupBox(self.widget1)
+        self.canGroupBox = QGroupBox(self.layoutWidget)
         self.canGroupBox.setObjectName(u"canGroupBox")
+        self.canGroupBox.setStyleSheet(u"QGroupBox {\n"
+"	border: 2px solid rgb(227, 30, 3);\n"
+"	border-radius: 10px;\n"
+"	margin-top: 0.5ex;\n"
+"	font-size: 24px;\n"
+"	font-weight: bold;\n"
+"	font: 700 20pt \"Uroob\";\n"
+"}\n"
+"\n"
+"QGroupBox::title {\n"
+"	color: rgb(227, 30, 3);\n"
+"	subcontrol-origin: margin;\n"
+"	subcontrol-position: top-left;\n"
+"	padding: 0 10px;\n"
+"	font-weight: bold;\n"
+"}")
         self.gridLayout_2 = QGridLayout(self.canGroupBox)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.canStatusNameLabel = QLabel(self.canGroupBox)
         self.canStatusNameLabel.setObjectName(u"canStatusNameLabel")
+        self.canStatusNameLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 1000 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout_2.addWidget(self.canStatusNameLabel, 0, 0, 1, 1)
 
         self.canSetButton = QPushButton(self.canGroupBox)
         self.canSetButton.setObjectName(u"canSetButton")
+        self.canSetButton.setStyleSheet(u"QPushButton {\n"
+"    background-color: rgb(33, 35, 39);\n"
+"    color: rgb(119, 118, 123);\n"
+"    font: bold 14px;\n"
+"    border-radius: 10px;\n"
+"    padding: 10px;\n"
+"	font: 700 14pt \"FreeMono\";\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: rgb(224, 0, 0); /* When clicked */\n"
+"    border-style: inset;\n"
+"    padding-left: 2px;\n"
+"    padding-top: 2px;\n"
+"}\n"
+"")
 
         self.gridLayout_2.addWidget(self.canSetButton, 1, 0, 1, 1)
 
         self.canResetButton = QPushButton(self.canGroupBox)
         self.canResetButton.setObjectName(u"canResetButton")
+        self.canResetButton.setStyleSheet(u"QPushButton {\n"
+"    background-color: rgb(33, 35, 39);\n"
+"    color: rgb(119, 118, 123);\n"
+"    font: bold 14px;\n"
+"    border-radius: 10px;\n"
+"    padding: 10px;\n"
+"	font: 700 14pt \"FreeMono\";\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: rgb(224, 0, 0); /* When clicked */\n"
+"    border-style: inset;\n"
+"    padding-left: 2px;\n"
+"    padding-top: 2px;\n"
+"}\n"
+"")
 
         self.gridLayout_2.addWidget(self.canResetButton, 1, 1, 1, 1)
 
         self.canStatusValueLabel = QLabel(self.canGroupBox)
         self.canStatusValueLabel.setObjectName(u"canStatusValueLabel")
+        self.canStatusValueLabel.setStyleSheet(u"color: rgb(119, 118, 123);\n"
+"font: 18pt \"FreeMono\";\n"
+"qproperty-alignment: AlignCenter;")
 
         self.gridLayout_2.addWidget(self.canStatusValueLabel, 0, 1, 1, 1)
 
 
         self.verticalLayout.addWidget(self.canGroupBox)
 
-        self.KillSwitchButton = QPushButton(self.widget1)
+        self.KillSwitchButton = QPushButton(self.layoutWidget)
         self.KillSwitchButton.setObjectName(u"KillSwitchButton")
         self.KillSwitchButton.setStyleSheet(u"QPushButton {\n"
 "    background-color: rgb(246, 11, 11);\n"
@@ -178,25 +307,26 @@ class Ui_guiMainWindow(object):
     def retranslateUi(self, guiMainWindow):
         guiMainWindow.setWindowTitle(QCoreApplication.translate("guiMainWindow", u"MainWindow", None))
         self.CurrentRoverStatus.setTitle(QCoreApplication.translate("guiMainWindow", u"Rover Status", None))
-        self.label_2.setText(QCoreApplication.translate("guiMainWindow", u"TextLabel", None))
-        self.currentSettingNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Current Setting:", None))
-        self.modeNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Mode:", None))
-        self.label.setText(QCoreApplication.translate("guiMainWindow", u"TextLabel", None))
+        self.modeNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Mode", None))
+        self.currentSettingNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Config", None))
+        self.label.setText(QCoreApplication.translate("guiMainWindow", u"1", None))
+        self.label_2.setText(QCoreApplication.translate("guiMainWindow", u"Drive", None))
         self.sensorDataPanel.setTitle(QCoreApplication.translate("guiMainWindow", u"Sensor Data", None))
         self.batteryNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Battery:", None))
         self.batteryValueLabel.setText(QCoreApplication.translate("guiMainWindow", u"?", None))
         self.LatencyNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Latency:", None))
         self.LatencyValueLabel.setText(QCoreApplication.translate("guiMainWindow", u"0 ms", None))
-        self.GPSNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"GPS Coordinates:", None))
-        self.LinearXNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Linear_x:", None))
+        self.GPSNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"GPS Coords", None))
+        self.LinearXNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Velocity", None))
         self.YawValueLabel.setText(QCoreApplication.translate("guiMainWindow", u"0 deg", None))
-        self.yawNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Yaw:", None))
+        self.yawNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"Yaw", None))
         self.LinearXValueLabel.setText(QCoreApplication.translate("guiMainWindow", u"0 m/s", None))
         self.GPSValuesLabel.setText(QCoreApplication.translate("guiMainWindow", u"0", None))
         self.canGroupBox.setTitle(QCoreApplication.translate("guiMainWindow", u"CAN", None))
         self.canStatusNameLabel.setText(QCoreApplication.translate("guiMainWindow", u"CAN Status:", None))
-        self.canSetButton.setText(QCoreApplication.translate("guiMainWindow", u"CAN Set", None))
-        self.canResetButton.setText(QCoreApplication.translate("guiMainWindow", u"CAN Reset", None))
+        self.canSetButton.setText(QCoreApplication.translate("guiMainWindow", u"SET", None))
+        self.canResetButton.setText(QCoreApplication.translate("guiMainWindow", u"RESET", None))
         self.canStatusValueLabel.setText(QCoreApplication.translate("guiMainWindow", u"UP", None))
         self.KillSwitchButton.setText(QCoreApplication.translate("guiMainWindow", u"Kill", None))
     # retranslateUi
+
